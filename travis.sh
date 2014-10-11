af19313d811cb6b6f770bd58927ff97e49ce94d2
xuwei-k <6b656e6a69@gmail.com>
2014-10-12 02:46:32 +0900

    use MySQL and PosgreSQL in travis test

--- travis.sh
+++ travis.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+rm scalikejdbc-core/src/test/resources/jdbc.properties &&
+cp -p scalikejdbc-core/src/test/resources/jdbc_$SCALIKEJDBC_DATABASE.properties scalikejdbc-core/src/test/resources/jdbc.properties &&
+
 if [[ ${TRAVIS_SCALA_VERSION} = "2.10.4" ]]; then
-  sbt ++${TRAVIS_SCALA_VERSION} test
+  sbt ++${TRAVIS_SCALA_VERSION} test:compile "project root211" testSequential
 elif [[ ${TRAVIS_SCALA_VERSION} = "scripted-test" ]]; then
   sbt '++ 2.11.2' root211/publishLocal '++ 2.10.4' publishLocal mapper-generator/scripted
 elif [[ ${TRAVIS_SCALA_VERSION} = "2.11.2" ]]; then
-  sbt ++${TRAVIS_SCALA_VERSION} root211/test
+  sbt ++${TRAVIS_SCALA_VERSION} root211/test:compile "project root211" testSequential
 else
   exit -1
 fi
