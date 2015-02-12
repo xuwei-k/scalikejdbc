3dc17441f7f086860d804b8229c0970ae1c8fbc8
xuwei-k <6b656e6a69@gmail.com>
2015-02-12 17:20:32 +0900

    wip

--- sbt.bat
+++ sbt.bat
@@ -1,2 +1,2 @@
 set SCRIPT_DIR=%~dp0
-java -Dfile.encoding=UTF8 -XX:ReservedCodeCacheSize=512M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=512M -Xmx2G -Xss2M -jar "%SCRIPT_DIR%\sbt-launch-0.13.5.jar" %*
+java -Dsbt.log.noformat=true -Dfile.encoding=UTF8 -XX:ReservedCodeCacheSize=512M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=512M -Xmx2G -Xss2M -jar "%SCRIPT_DIR%\sbt-launch-0.13.5.jar" %*
