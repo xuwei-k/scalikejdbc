6b77a4cb6beebc344e100866bdabb0d49a2a23c4
xuwei-k <6b656e6a69@gmail.com>
2016-09-15 00:38:21 +0900

    Scala 2.12.0-RC1

--- scripts/release_2.12.sh
+++ scripts/release_2.12.sh
@@ -3,14 +3,14 @@
 cd `dirname $0`/..
 rm -rf */target
 
-sbt 'set scalaVersion := "2.12.0-M5"' \
+sbt 'set scalaVersion := "2.12.0-RC1"' \
   clean \
-  "project core" 'set scalaVersion := "2.12.0-M5"' publishSigned \
-  "project config" 'set scalaVersion := "2.12.0-M5"' publishSigned \
-  "project interpolation-macro" 'set scalaVersion := "2.12.0-M5"' publishSigned \
-  "project interpolation" 'set scalaVersion := "2.12.0-M5"' publishSigned \
-  "project library" 'set scalaVersion := "2.12.0-M5"' publishSigned \
-  "project jsr310" 'set scalaVersion := "2.12.0-M5"' publishSigned \
-  "project syntax-support-macro" 'set scalaVersion := "2.12.0-M5"' publishSigned \
-  "project test" 'set scalaVersion := "2.12.0-M5"' publishSigned \
-  "project mapper-generator-core" 'set scalaVersion := "2.12.0-M5"' publishSigned
+  "project core" 'set scalaVersion := "2.12.0-RC1"' publishSigned \
+  "project config" 'set scalaVersion := "2.12.0-RC1"' publishSigned \
+  "project interpolation-macro" 'set scalaVersion := "2.12.0-RC1"' publishSigned \
+  "project interpolation" 'set scalaVersion := "2.12.0-RC1"' publishSigned \
+  "project library" 'set scalaVersion := "2.12.0-RC1"' publishSigned \
+  "project jsr310" 'set scalaVersion := "2.12.0-RC1"' publishSigned \
+  "project syntax-support-macro" 'set scalaVersion := "2.12.0-RC1"' publishSigned \
+  "project test" 'set scalaVersion := "2.12.0-RC1"' publishSigned \
+  "project mapper-generator-core" 'set scalaVersion := "2.12.0-RC1"' publishSigned
