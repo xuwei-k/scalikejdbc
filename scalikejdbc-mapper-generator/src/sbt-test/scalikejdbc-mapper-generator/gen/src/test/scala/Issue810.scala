a98b48fe98515da804e3d5f5311cc78bcc580a93
xuwei-k <6b656e6a69@gmail.com>
2018-01-11 23:16:34 +0900

    issue 810

---
+++ scalikejdbc-mapper-generator/src/sbt-test/scalikejdbc-mapper-generator/gen/src/test/scala/Issue810.scala
@@ -0,0 +1,14 @@
+package app.models
+
+import org.scalatest._
+import scalikejdbc.scalatest.AutoRollback
+
+class Issue810 extends fixture.FlatSpec with Matchers with AutoRollback {
+  behavior of "AddressStreet"
+
+  it should "be available" in { implicit session =>
+    AddressStreet.findAll().toSet should equal(Set())
+    AddressStreet.find(-1) should equal(None)
+    AddressStreet.countAll() should equal(0)
+  }
+}
